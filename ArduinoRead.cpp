faa1c2995b32804f75d86b4d7cf4b6834e0460cb
danstaples <daniel_staples93@hotmail.com>
2019-02-28 16:46:29 +0000

    Update ArduinoRead.cpp
    Updated file to accurately reflect what is on Jetson

--- ArduinoRead.cpp
+++ ArduinoRead.cpp
@@ -7,6 +7,7 @@
 #include <termios.h>
 #include <errno.h>
 #include <sys/ioctl.h>
+#include <stdbool.h>
 
 #define BUFFER_SIZE 1024
 
@@ -17,6 +18,7 @@ int main(int argc, char *argv[])
 {
   int fd, n, i;
   char buf[BUFFER_SIZE] = "temp text";
+  bool ready = 0;
   
   struct termios toptions;
 
@@ -30,8 +32,8 @@ int main(int argc, char *argv[])
   /* get current serial port settings */
   tcgetattr(fd, &toptions);
   /* set 9600 baud both ways */
-  cfsetispeed(&toptions, B9600);
-  cfsetospeed(&toptions, B9600);
+  cfsetispeed(&toptions, B115200);
+  cfsetospeed(&toptions, B115200);
   /* 8 bits, no parity, no stop bits */
   toptions.c_cflag &= ~PARENB;
   toptions.c_cflag &= ~CSTOPB;
@@ -41,18 +43,46 @@ int main(int argc, char *argv[])
   toptions.c_lflag |= ICANON;
   /* commit the serial port settings */
   tcsetattr(fd, TCSANOW, &toptions);
-	while(){
-	bool ready = 0
-	n = read(fd, buf, BUFFER_SIZE);
-	if (n == 239) {
-	write(fd, "1", 1);
-	}
-  /* Receive string from Arduino */
-	// n = read(fd, buf, BUFFER_SIZE);
-  /* insert terminating zero in the string */
-	buf[n] = 0;
+	while(1){
+	
+		n = read(fd, buf, BUFFER_SIZE);
+		buf[n] = 0;
+		if(n!=1)
+		printf("%i bytes read, buffer contains: %s\n", n, buf);
 
-	printf("%i bytes read, buffer contains: %s\n", n, buf);
-	usleep(100000);
+		//printf("Ready status: %i\n", ready);
+
+
+		if ((buf[0] == '9') && (n == 2)) 
+		{ready = 1;}
+		
+		if(ready == 1){
+	
+			printf("\nSending 1\n");
+			write(fd, "1", 1);
+			tcdrain(fd); // Orders jetson to wait until its send buffer clears, prevents funky serial outputs
+			ready = 0;
+			printf("Ready status: %i\n", ready);
+			//usleep(1000);
+		}
+		
+		if (n > 40)
+		{
+			printf("DATA: %s",buf);
+			ready = 1;
+		}
+
+		//for (int i = 0; i < n; i++){
+		//	buf[i] = '\0' ;
+		//}
+
+		// tcflush(fd, tcioflush);
+		
+	
+	  /* Receive string from Arduino */
+		// n = read(fd, buf, BUFFER_SIZE);
+	  /* insert terminating zero in the string */
+		//printf("%i bytes read, buffer contains: %s\n", n, buf);
+		//usleep(100000);
 	}
 }
